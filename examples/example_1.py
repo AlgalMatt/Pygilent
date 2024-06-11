d15214f50ff09bc431c52a94124da049e62250b4
Matt_D <algalmatt@gmail.com>
2024-06-11 12:42:37 +0100

    added first example

---
+++ examples/example_1.py
@@ -0,0 +1,27 @@
+import Pygilent as pyg
+import os
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+#get the default standard values
+stndvals=pyg.stnds.get_default_stndvals()
+
+#import the batch from folder
+dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+new_batch=pyg.pygilent.import_batch(dir_path/'example_batch_TEratio.b')
+
+
+new_batch.set_blks(how='auto')
+#new_batch.check_blks()
+new_batch.set_brkt_stnds(how='auto', keyword='stgfrm')
+new_batch.cali_mode='ratio curve'
+new_batch.set_ratio_iso(how='auto', keyword='Ca48')
+new_batch.set_cali_stnds(stndvals, how='ui')
+
+new_batch.initialise()
+new_batch.blank_correction()
+new_batch.ratio_correction()
+#Why does bracket correction produce non 1's for STGFrm??
+new_batch.bracket_correction()
+new_batch.calibrate()
